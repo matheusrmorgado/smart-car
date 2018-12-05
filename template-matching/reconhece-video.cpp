58703d42523cc576bbee80c6a5937aa5b29835eb
matheusrmorgado <matheus.morgado@usp.br>
2018-12-04 22:38:14 -0200

    Update video template matching detection

--- template-matching/reconhece-video.cpp
+++ template-matching/reconhece-video.cpp
@@ -5,18 +5,18 @@ void printSQR( Mat_<COR>& img, Mat_<FLT> quadrado){
 	Mat_<FLT> imgFLT(img.size());
 	Mat_<FLT> result;
 	
+	#pragma omp parallel for
 	for (int ii = 0; ii  < img.rows; ii++)
 		for (int jj = 0; jj  < img.cols; jj++)
 			imgFLT(ii,jj) = (float)(img(ii,jj)[0])/256;
-		
-		
+
 	double minVal; double maxVal;
 	Point minLoc; Point maxLoc; Point matchLoc;
 
 	int tamanho; float aux = 0.0;
 	
 	#pragma omp parallel for
-	for ( int s = 15; s < 61; s = s + 6){
+	for ( int s = 15; s < 60; s=s+5){
 		resize( quadrado, quadrado, Size(s,s), 0, 0, INTER_AREA );
 		quadrado = somaAbsDois( dcReject( quadrado ) );
 		matchTemplate( imgFLT, quadrado, result, CV_TM_CCORR );
@@ -28,11 +28,33 @@ void printSQR( Mat_<COR>& img, Mat_<FLT> quadrado){
 		}
 	}
 
-	
-	if( aux > 0.23f){
-		retang(img, matchLoc.y,matchLoc.x-1, matchLoc.y-1 + tamanho-1,matchLoc.x + tamanho-1, COR(255,0,0));
-	}
+	resize( quadrado, quadrado, Size(tamanho,tamanho), 0, 0, INTER_AREA );
+	quadrado = somaAbsDois( dcReject( quadrado ) );
+	matchTemplate( imgFLT, quadrado, result, CV_TM_CCORR );
+	minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc );
 
+	if( tamanho > 20 && maxVal > 0.23f){
+		line( img, Point( matchLoc.x + quadrado.cols/2 , matchLoc.y + quadrado.rows/2 + 0.12*quadrado.rows ), 
+			Point( matchLoc.x + quadrado.cols/2 , matchLoc.y + quadrado.rows/2 - 0.12*quadrado.rows ), Scalar(0,0,255), 1, 8);
+		line( img, Point( matchLoc.x + quadrado.cols/2 + 0.12*quadrado.rows , matchLoc.y + quadrado.rows/2 ), 
+			Point( matchLoc.x + quadrado.cols/2 - 0.12*quadrado.rows , matchLoc.y + quadrado.rows/2 ), Scalar(0,0,255), 1, 8);
+		rectangle( img, matchLoc, Point( matchLoc.x + quadrado.cols , matchLoc.y + quadrado.rows ), Scalar(0,0,255), 1.5, 8);
+
+		string str = to_string(maxVal);
+		putTxt( img, matchLoc.y + quadrado.rows/2 + 35, matchLoc.x + quadrado.cols/2 + 2, 
+			str, COR(255,0,0) , 1, true, COR(0,0,0));
+	}
+	else if( tamanho >= 15 && tamanho <= 20 && maxVal > 0.16f){
+		line( img, Point( matchLoc.x + quadrado.cols/2 , matchLoc.y + quadrado.rows/2 + 0.12*quadrado.rows ), 
+			Point( matchLoc.x + quadrado.cols/2 , matchLoc.y + quadrado.rows/2 - 0.12*quadrado.rows ), Scalar(0,0,255), 1, 8);
+		line( img, Point( matchLoc.x + quadrado.cols/2 + 0.12*quadrado.rows , matchLoc.y + quadrado.rows/2 ), 
+			Point( matchLoc.x + quadrado.cols/2 - 0.12*quadrado.rows , matchLoc.y + quadrado.rows/2 ), Scalar(0,0,255), 1, 8);
+		rectangle( img, matchLoc, Point( matchLoc.x + quadrado.cols , matchLoc.y + quadrado.rows ), Scalar(0,0,255), 1.5, 8);
+
+		string str = to_string(maxVal);
+		putTxt( img, matchLoc.y + quadrado.rows/2 + 35, matchLoc.x + quadrado.cols/2 + 2, 
+			str, COR(255,0,0) , 1, true, COR(0,0,0));
+	}
 }
 	
 int main( int argc, char** argv )
@@ -71,10 +93,11 @@ int main( int argc, char** argv )
 		
 		vi >> src;
 		
-		if (i == 450) 
-			imp( src, "quad450-detection.png" ); 
+		if (i == 450) imp( src, "quad450.png" ); 
 
 		printSQR(src, quadrado); 
+
+		if (i == 450) imp( src, "quad450-detection.png" ); 
 
 		//Show results
 		namedWindow("Template Matching Video", 0);
